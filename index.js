0fc64cfab55a2fbcac21bb238d4ebaf8ff6b71a7
altayozyavas <altayozyavas@gmail.com>
2022-05-20 20:44:55 -0500

    add functionality to mole game

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// your code 
+const holes  = document.getElementsByClassName("hole");
+const gameZone = document.getElementById("hit-a-mole");
+let score = 0;
+
+gameZone.addEventListener("click", (clickEvent) => {
+    const randomHoleIndex = Math.floor(Math.random() * holes.length);
+    holes[randomHoleIndex].classList.toggle("mole");
+    if (clickEvent.target.matches(".mole")) {
+        score++;  
+    } 
+    document.getElementById("score").innerHTML = score;
+});
